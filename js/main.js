a590dfba2f70a6fa49e97d0cdb88cfba785ac611
Joe Toscano <yo@realjoet.me>
2018-03-29 10:49:56 -0600

    updated html for js progress, added js functionality but glitchy upon open/close

--- js/main.js
+++ js/main.js
@@ -1,5 +1,31 @@
 $(function() {
-  $('.not-available').click(function(){
-    alert('Chapter not available yet, but keep checking back!')
+  var footerHeight = $('footer').css("height");
+  console.log('before any actions, footerHeight = ' + footerHeight);
+
+
+  $('.footer--menu-button').click(function(){
+    var $this = $(this);
+    var $footerHeight = $('footer').height();;
+    var $footerReset = (-1) * $footerHeight;
+
+    if ($this.hasClass('open')) {
+      $('footer').animate({
+        position: 'absolute',
+        bottom: $footerReset + 10,
+      }, 500);
+
+      $this.html('Open Menu');
+
+      $this.removeClass('open');
+    } else {
+      $('footer').animate({
+        position: 'absolute',
+        bottom: '0',
+      }, 500);
+
+      $this.html('Close Menu');
+
+      $this.addClass('open');
+    }
   });
 });
